8a358a7380c341918ec92ec185a2a31c5359a9bc
artmexbet <60144997+artmexbet@users.noreply.github.com>
2020-12-15 22:29:16 +0700

    init

---
+++ TheGameProject/Assets/Scripts/UI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class UI : MonoBehaviour
+{
+    public int seed;
+    private bool _played = false;
+    private GameManager _gm;
+
+    private void Awake()
+    {
+        seed =  Random.Range(1000, 9999);
+        _gm = GameObject.Find("Generator").GetComponent<GameManager>();
+    }
+
+    public void Generate()
+    {
+        _gm.Generate(seed);
+    }
+
+    public void Play(GameObject eventObject)
+    {
+        if (_played)
+        {
+            eventObject.GetComponentInChildren<Text>().text = "Play";
+            print("Stopped");
+        }
+        else
+        {
+            eventObject.GetComponentInChildren<Text>().text = "Stop";
+            print("Played");
+            
+        }
+        _played = !_played;
+    }
+}
