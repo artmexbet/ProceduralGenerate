0f19474eba0bff67d9ca1c1781819b80ccc634eb
artmexbet <60144997+artmexbet@users.noreply.github.com>
2020-12-17 16:28:24 +0700

    Добавил возможность создавать объекты
    Осталось сделать сетку и сохранение этих объектов на сетке

--- TheGameProject/Assets/Scripts/UI.cs
+++ TheGameProject/Assets/Scripts/UI.cs
@@ -10,11 +10,14 @@ public class UI : MonoBehaviour
     public int seed;
     private bool _played = false;
     private GameManager _gm;
+    private Building _flyingBuilding;
+    private Camera _mainCamera;
 
     private void Awake()
     {
         seed =  Random.Range(1000, 9999);
         _gm = GameObject.Find("Generator").GetComponent<GameManager>();
+        _mainCamera = Camera.main;
     }
 
     public void Generate()
@@ -36,5 +39,28 @@ public class UI : MonoBehaviour
             
         }
         _played = !_played;
+    }
+
+    public void SpawnTile(Building prefab)
+    {
+        if (_flyingBuilding != null)
+        {
+            Destroy(_flyingBuilding);
+        }
+        _flyingBuilding = Instantiate(prefab);
+    }
+
+    private void Update()
+    {
+        if (_flyingBuilding != null)
+        {
+            var groundPlane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (groundPlane.Raycast(ray, out float position))
+            {
+                Vector3 worldPosition = ray.GetPoint(position);
+                _flyingBuilding.transform.position = worldPosition;
+            }
+        }
     }
 }
